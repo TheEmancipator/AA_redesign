59f89b101583d43a6f82096b34fd2cd2350642ca
TheEmancipator <chaximeer@kaist.ac.kr>
2022-02-09 14:39:35 +0900

    remove unnecessary methods

--- VideoRental/Video.java
+++ VideoRental/Video.java
@@ -1,15 +1,16 @@
 import java.util.Date;
 
 public abstract class Video {
-    public static final int VHS = 1;
-    public static final int DVD = 2;
-    public static final int CD = 3;
-    public static final int REGULAR = 1;
-    public static final int NEW_RELEASE = 2;
+    private int priceCode ;
+    public static final int REGULAR = 1 ;
+    public static final int NEW_RELEASE =2 ;
+
+    public static final int VHS = 1 ;
+    public static final int CD = 2 ;
+    public static final int DVD = 3 ;
 
     protected Date registeredDate;
     private String title;
-    private int priceCode;
     private boolean rented;
 
     public Video(Date registeredDate) {
@@ -41,13 +42,4 @@ public abstract class Video {
     public void setRented(boolean rented) {
         this.rented = rented;
     }
-
-    public Date getRegisteredDate() {
-        return registeredDate;
-    }
-
-    public void setRegisteredDate(Date registeredDate) {
-        this.registeredDate = registeredDate;
-    }
-
 }
